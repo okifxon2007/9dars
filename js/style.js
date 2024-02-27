9c67a39364c4866c1950122e98759fa7077e2d76
okifxon <shokirjonovokifxon46@gmail.com>
2024-02-28 00:21:06 +0500

    dars

---
+++ js/style.js
@@ -0,0 +1,12 @@
+let form = document.querySelector('#form');
+let name = document.querySelector('#name');
+let price = document.querySelector('#price');
+let status = document.querySelector('#status');
+let deck = document.querySelector('#deck');
+
+function creat(row) {
+    return `
+    
+    `
+    
+}
